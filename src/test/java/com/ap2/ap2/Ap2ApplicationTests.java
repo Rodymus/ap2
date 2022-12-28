5e58b00e4ab038d3231beb824fae7b374611a345
Rodymus <robertogonzalez.ar@gmail.com>
2022-12-28 01:07:49 -0300

    Inicio

---
+++ src/test/java/com/ap2/ap2/Ap2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ap2.ap2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ap2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
